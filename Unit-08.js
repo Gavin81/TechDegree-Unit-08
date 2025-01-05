0feebef0d5ddadc1cb02e24e0e80f5549620635d
Gavin81 <g_eyquem@hotmail.com>
2025-01-05 17:39:03 +0000

    Update Unit-08.js

--- Unit-08.js
+++ Unit-08.js
@@ -1,7 +1,13 @@
 let employees = [];
-let urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`;
-let gridContainer = document.querySelector('.grid-container');
-let overlay = document.querySelector('.modal');
-let modalContainer = document.querySelector('.modal-content');
-let modalCLose = document.querySelector('.modal-close');
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`;
+const gridContainer = document.querySelector('.grid-container');
+const overlay = document.querySelector('.modal');
+const modalContainer = document.querySelector('.modal-content');
+const modalCLose = document.querySelector('.modal-close');
 
+// fetch data from API
+fetch(urlAPI)
+    .then(res => res.json())
+    .then(res => res.results)
+    .then(displayEmployees)
+    .catch(err => console.log(err))
