df50347adbbcc571a0dea7856e3a81cf213871e3
Gavin81 <g_eyquem@hotmail.com>
2025-01-06 14:37:26 +0000

    css, html and js updates

--- Unit-08.js
+++ Unit-08.js
@@ -11,3 +11,79 @@ fetch(urlAPI)
     .then(res => res.results)
     .then(displayEmployees)
     .catch(err => console.log(err))
+
+
+
+// Employee function that displays employee data
+
+function displayEmployees(employeeData){
+    employees = employeeData;
+    let employeeHTML = '';
+
+    // This Loops through each employee
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+
+
+        // Below is the template literal
+        employeeHTML += `
+            <div class="card" data-index="${index}">
+                <img class="avatar" src="${picture.large}" />
+                <div class="text-container">
+                    <h2 class="name">${name.first} ${name.last}</h2>
+                    <p class="email">${email}</p>
+                    <p class="address">${city}</p>
+                </div>
+            </div>
+        `
+    });
+
+    gridContainer.innerHTML = employeeHTML;
+}
+
+
+// The modal display function
+function displayModal(index){
+    let {name, dob, phone, email, location:{city, street, state, postcode}, picture} = employees[index];
+
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class="address">${street}, ${state} ${postcode}</p>
+            <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+    `;
+
+
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
+
+
+gridContainer.addEventListener('click', e => {
+    // The click is not on the gridContainer itself
+    if(e.target !== gridContainer){
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+
+modalCLose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
+
+
+index.html.remove(employeeHTML && modalHTML);
